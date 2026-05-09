663f613af6b2fbb3ca522807f94799092106c914
venkat2222222 <omnivers003@gmail.com>
2026-05-09 10:31:05 +0530

    first commit

---
+++ complaintbot/src/main/java/com/complaint/complaintbot/ComplaintbotApplication.java
@@ -0,0 +1,13 @@
+package com.complaint.complaintbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintbotApplication.class, args);
+	}
+
+}
---
+++ complaintbot/src/test/java/com/complaint/complaintbot/ComplaintbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complaint.complaintbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
